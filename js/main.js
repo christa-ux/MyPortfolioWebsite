8ba8bbe0a82d0d4ba18f90ddb2b2da27daf47b3d
christa <christa.arraj@gmail.com>
2024-01-02 15:40:48 -0500

    fixed skills table

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
+
+
+/*
+//navbar
 document.addEventListener("DOMContentLoaded", function(){
     // add padding top to show content behind navbar
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
+}); 
+*/
+
+
+//header
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch the header content and inject it into the container
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('headerContainer').innerHTML = data;
+        })
+        .catch(error => console.error('Error fetching header:', error));
 });
+
+
+
+//footer
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch the header content and inject it into the container
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footerContainer').innerHTML = data;
+        })
+        .catch(error => console.error('Error fetching footer:', error));
+});
+
+
+
+//contact form buttons
+  
